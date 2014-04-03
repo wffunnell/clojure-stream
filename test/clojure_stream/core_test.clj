97f98b72ababb8774e007be242d2df8f19f545a5
Will Funnell <w.f.funnell@gmail.com>
2014-04-03 07:12:03 +0100

    Initial commit

---
+++ test/clojure_stream/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-stream.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-stream.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
